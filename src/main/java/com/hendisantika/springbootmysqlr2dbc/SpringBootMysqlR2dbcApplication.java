ab4a79c61e2852e0138aaf7c02d2fc5b6c224cac
hendisantika <hendisantika@yahoo.co.id>
2023-06-14 05:44:13 +0700

    #6 - Add initializer in Main Class ✏️📗:octocat:⬆

--- src/main/java/com/hendisantika/springbootmysqlr2dbc/SpringBootMysqlR2dbcApplication.java
+++ src/main/java/com/hendisantika/springbootmysqlr2dbc/SpringBootMysqlR2dbcApplication.java
@@ -1,13 +1,29 @@
 package com.hendisantika.springbootmysqlr2dbc;
 
+import io.r2dbc.spi.ConnectionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
 
 @SpringBootApplication
 public class SpringBootMysqlR2dbcApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootMysqlR2dbcApplication.class, args);
+    }
+
+    @Bean()
+    ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
+        ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+        initializer.setConnectionFactory(connectionFactory);
+        // This will create our database table and schema
+        initializer.setDatabasePopulator(new ResourceDatabasePopulator(new ClassPathResource("dbsetup.sql")));
+        // This will drop our table after we are done so we can have a fresh start next run
+        initializer.setDatabaseCleaner(new ResourceDatabasePopulator(new ClassPathResource("cleanup.sql")));
+        return initializer;
     }
 
 }
