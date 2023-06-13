6948b90a68be5ca61c399a58d5abbf7889f9748a
hendisantika <hendisantika@yahoo.co.id>
2023-06-14 05:36:20 +0700

    #1 - Initial Commit ✏️📗:octocat:⬆

---
+++ src/main/java/com/hendisantika/springbootmysqlr2dbc/SpringBootMysqlR2dbcApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootmysqlr2dbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMysqlR2dbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMysqlR2dbcApplication.class, args);
+    }
+
+}
